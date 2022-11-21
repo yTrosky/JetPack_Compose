05e71f1e75c90f925b2f7105f14c8f791ff34022
yTrosky <thiago.machado2498@gmail.com>
2022-11-21 09:55:04 -0300

    Atualização

--- app/src/main/java/com/example/aulateste/MainActivity.kt
+++ app/src/main/java/com/example/aulateste/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.aulateste
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
@@ -12,25 +14,31 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.aulateste.ui.theme.AulaTesteTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //setContent: bloco que define o layout do app a partir da funções
         setContent {
-            MessageCard(Message("Android" , "Jetpack Compose")) //metódo para exibir as mensagens que vão ser exibidas
+            AulaTesteTheme() {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("Android", "Jetpack Compose"))
+                }
+            }
         }
+
     }
 }
-
 data class Message(val author: String, val body: String)
 
-@Composable //anotação que permite que funções compostas sejam criadas
-fun MessageCard(msg: Message) { //função criada para a visualização da função "MessageCard"
-
+//notação para tornar uma função composta(uma função chama a outra)
+@Composable
+fun MessageCard(msg: Message) {
+    //função Text
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(R.drawable.profile_picture),
@@ -38,21 +46,45 @@ fun MessageCard(msg: Message) { //função criada para a visualização da funç
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.primary, CircleShape)
         )
-        Spacer(modifier = Modifier.height(4.dp)) //função de outra biblioteca que serve para modificar as dimensões da imagem
 
+        Spacer(modifier = Modifier.width(8.dp))
 
-        //função para deixar os elementos na verticl, em colunas
         Column {
-            Text(text = msg.author)
-            Text(text = msg.body)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondary,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp){
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.subtitle1
+                )
+            }
         }
     }
 }
-@Preview //anotação que permite a visualização das funções de composição, sem a necessidade de usar emuladores
-@Composable //anotação que permite que funções compostas sejam criadas
-fun PreviewMessageCard() { //função criada para a visualização da função "MessageCard"
-    MessageCard(
-        msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
-    )
+
+//Notação que permite visualizar o resultado sem precisar emular o app (só pode ser usada com funções sem parâmetro)
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+
+@Composable
+fun PreviewMessageCard() {
+    AulaTesteTheme() {
+        Surface{
+            MessageCard(
+                msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
+            )
+        }
+    }
 }
